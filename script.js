6ab56a8fb767202eb1be5ba8b396096c9241d472
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-03 16:51:24 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const weatherForm = document.querySelector("#weather-form");
+const cityNameInput = document.querySelector("#city-name-input");
+const weatherCity = document.querySelector("#weather-city");
+const weatherTemp = document.querySelector("#weather-temp");
+const weatherFeelsLike = document.querySelector("#weather-feels-like");
+const weatherDescription = document.querySelector("#weather-description");
+const weatherHighLow = document.querySelector("#weather-high-low");
+
+weatherForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    weatherCity.textContent = "Loading...";
+    weatherTemp.textContent = "";
+    weatherFeelsLike.textContent = "";
+    weatherDescription.textContent = "";
+    weatherHighLow.textContent = "";
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${searchtext}&units=imperial&appid=test`
+        );
+        if (response.ok) {
+            const data = await response.json();
+            weatherCity.textContent = data.name;
+            weatherTemp.textContent = `Current Temp: ${data.main.temp} degrees`;
+            weatherFeelsLike.textContent = `(Feels like ${data.main.feels_like})`;
+            weatherDescription.textContent = `Description: ${data.weather[0].description}`;
+            weatherHighLow.textContent = `High: ${data.main.temp_max} degrees / Low: ${data.main.temp_min}`;
+        } else {
+            if (response.status === 400) {
+                throw new Error(
+                    "Please do not include special characters in your request"
+                );
+            } else {
+                throw new Error(
+                    "Not found, please make sure the city name is spelled correctly"
+                );
+            }
+        }
+    } catch (err) {
+        weatherCity.textContent = err;
+        console.error(err);
+    }
+    cityNameInput.value = "";
+});
